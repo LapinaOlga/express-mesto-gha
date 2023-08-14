8207970ca23ad2115f68419ad010514163f1cf4a
LapinaOlga <121168625+LapinaOlga@users.noreply.github.com>
2023-08-14 21:33:15 +0200

    Update httpCodes.js

--- enums/httpCodes.js
+++ enums/httpCodes.js
@@ -1,4 +1,4 @@
-export const HTTP_CREATED = 201;
-export const HTTP_BAD_REQUEST = 400;
-export const HTTP_NOT_FOUND = 404;
-export const HTTP_INTERNAL_ERROR = 500;
+module.exports.HTTP_CREATED = 201;
+module.exports.HTTP_BAD_REQUEST = 400;
+module.exports.HTTP_NOT_FOUND = 404;
+module.exports.HTTP_INTERNAL_ERROR = 500;
