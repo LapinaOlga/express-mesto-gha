65597a95b1acc2869722e0e6671328a9fa32aa4b
LapinaOlga <121168625+LapinaOlga@users.noreply.github.com>
2023-08-20 23:58:54 +0200

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -112,11 +112,9 @@ module.exports.login = async (req, res, next) => {
     if (!bcrypt.compareSync(password, user.password)) {
       throw new UnauthorizedError();
     }
-
-    // Повторяем ответ от сервера Яндекса
-    res.send({
-      token: jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' }),
-    });
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+    res.cookie('jwt', token, { maxAge: 10800, sameSite: true })
+      .send({ message: 'Успешно' });
   } catch (e) {
     next(e);
   }
