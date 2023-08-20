8cdff523f2c378a1585037042fcfd44f112b9d4b
LapinaOlga <121168625+LapinaOlga@users.noreply.github.com>
2023-08-20 23:24:14 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,38 +5,23 @@ const {
 } = require('../controllers/users');
 
 router.get('', celebrate({
-  headers: {
-    authorization: Joi.string().required().regex(/^Bearer /i),
-  },
 }), getAllUsers);
 router.get('/me', celebrate({
-  headers: {
-    authorization: Joi.string().required().regex(/^Bearer /i),
-  },
 }), getCurrentUser);
 router.patch('/me', celebrate({
   body: {
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
   },
-  headers: {
-    authorization: Joi.string().required().regex(/^Bearer /i),
-  },
 }), updateCurrentUser);
 router.patch('/me/avatar', celebrate({
   body: {
     avatar: Joi.string().required().uri(),
   },
-  headers: {
-    authorization: Joi.string().required().regex(/^Bearer /i),
-  },
 }), updateCurrentUserAvatar);
 router.get('/:id', celebrate({
   params: {
     id: Joi.string().required().alphanum().length(24),
-  },
-  headers: {
-    authorization: Joi.string().required().regex(/^Bearer /i),
   },
 }), getUserById);
 
