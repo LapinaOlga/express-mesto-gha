9f1d37d3ff78d90c893e3bcdd14cd33c404db4c1
LapinaOlga <121168625+LapinaOlga@users.noreply.github.com>
2023-08-21 00:04:36 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const { celebrate, Joi } = require('celebrate');
 const {
   getUserById, getAllUsers, updateCurrentUser, updateCurrentUserAvatar, getCurrentUser,
 } = require('../controllers/users');
+const { validateUrl } = require('../utils/validateUrl');
 
 router.get('', getAllUsers);
 router.get('/me', getCurrentUser);
@@ -14,7 +15,12 @@ router.patch('/me', celebrate({
 }), updateCurrentUser);
 router.patch('/me/avatar', celebrate({
   body: {
-    avatar: Joi.string().required().uri(),
+    avatar: Joi.string().required().custom((value, helper) => {
+      if (validateUrl(value)) {
+        return true;
+      }
+      return helper.message('Поле avatar содержит невалидный URL');
+    }),
   },
 }), updateCurrentUserAvatar);
 router.get('/:id', celebrate({
