86306cb8f233200727b43a3b01287f47c2127014
LapinaOlga <121168625+LapinaOlga@users.noreply.github.com>
2023-08-20 21:48:52 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,16 +5,14 @@ const { validateUrl } = require('../utils/validateUrl');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Поле name не может быть короче 2х символов'],
+    maxlength: [30, 'Поле name не может быть длиннее 30 символов'],
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
-    minLength: 2,
-    maxLength: 30,
+    minlength: [2, 'Поле about не может быть короче 2х символов'],
+    maxlength: [30, 'Поле about не может быть длиннее 30 символов'],
     default: 'Исследователь океана',
   },
   avatar: {
@@ -22,23 +20,23 @@ const userSchema = new mongoose.Schema({
     default: 'https://randomwordgenerator.com/img/picture-generator/52e1d6444b56b10ff3d8992cc12c30771037dbf85254784b772872d2944e_640.jpg',
     validate: {
       validator: validateUrl,
-      message: (props) => `${props.value} is not a valid url!`,
+      message: 'Поле avatar должно быть валидным URL',
     },
   },
   email: {
     type: String,
-    required: true,
+    required: [true, 'Email обязателен для заполнения'],
     unique: true,
     validate: {
       validator(v) {
         return validator.isEmail(v);
       },
-      message: (props) => `${props.value} is not a valid email!`,
+      message: 'Поле email должно быть валидным email-адресом',
     },
   },
   password: {
     type: String,
-    required: true,
+    required: [true, 'Пароль обязателен для заполнения'],
     select: false,
   },
 });
