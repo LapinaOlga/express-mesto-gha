e2779be0d5ec71377149c3f181fece1ef90f434a
LapinaOlga <121168625+LapinaOlga@users.noreply.github.com>
2023-08-27 20:53:51 +0200

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const ProtectedRouteError = require('../errors/ProtectedRouteError');
+const User = require('../models/user');
+
+module.exports.authMiddleware = async (req, res, next) => {
+  if (['/signin', '/signup'].includes(req.url)) {
+    next();
+    return;
+  }
+
+  const token = req.cookies.jwt;
+
+  if (!token) {
+    next(new ProtectedRouteError());
+    return;
+  }
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    next(new ProtectedRouteError());
+    return;
+  }
+
+  try {
+    // Пользователь может быть удален, а JWT токен при этом будет активным.
+    // Дополнительно проверяем этот кейс.
+    const user = await User.findById(payload._id);
+
+    if (!user) {
+      throw new ProtectedRouteError();
+    }
+
+    req.user = user;
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
