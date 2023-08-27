56fca4ec7b0fcea5f78c12d6edf344ab283ce8cf
LapinaOlga <121168625+LapinaOlga@users.noreply.github.com>
2023-08-27 21:38:16 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,16 +4,17 @@ const {
   getUserById, getAllUsers, updateCurrentUser, updateCurrentUserAvatar, getCurrentUser,
 } = require('../controllers/users');
 const { validateUrl } = require('../utils/validateUrl');
+const authMiddleware = require('../middleware/auth');
 
-router.get('', getAllUsers);
-router.get('/me', getCurrentUser);
-router.patch('/me', celebrate({
+router.get('', authMiddleware, getAllUsers);
+router.get('/me', authMiddleware, getCurrentUser);
+router.patch('/me', authMiddleware, celebrate({
   body: {
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
   },
 }), updateCurrentUser);
-router.patch('/me/avatar', celebrate({
+router.patch('/me/avatar', authMiddleware, celebrate({
   body: {
     avatar: Joi.string().required().custom((value, helper) => {
       if (validateUrl(value)) {
@@ -23,7 +24,7 @@ router.patch('/me/avatar', celebrate({
     }),
   },
 }), updateCurrentUserAvatar);
-router.get('/:id', celebrate({
+router.get('/:id', authMiddleware, celebrate({
   params: {
     id: Joi.string().required().alphanum().length(24),
   },
